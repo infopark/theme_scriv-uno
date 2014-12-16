5b90b07924693e42edc4c300198319d04cfc5a6e
Karina <kt@jetaido.com>
2014-12-16 16:56:28 +0100

    open flap upon login issues, keep obj

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,16 +3,16 @@ class SessionsController < ApplicationController
   def create
     if valid_credentials?(params[:login], params[:password])
       session[:user] = params[:login]
-      redirect_to scrivito_path(Obj.homepage)
+      redirect_to scrivito_path(Obj.find(params[:showing_obj]))
     else
-      flash[:alert] = "The credentials you provided are invalid"
-      redirect_to scrivito_path(Obj.homepage)
+      flash[:alert] = "LOGIN: The credentials you provided are invalid"
+      redirect_to scrivito_path(Obj.find(params[:showing_obj]))
     end
   end
 
   def destroy
     session[:user] = nil 
-    redirect_to scrivito_path(Obj.homepage) 
+    redirect_to scrivito_path(Obj.homepage)
   end
 
 
