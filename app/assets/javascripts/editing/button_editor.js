70d6b27335ba11e487896f42c68c91fc66bbe794
Karina <kt@jetaido.com>
2014-12-29 16:18:33 +0100

    indicating selected option in button_editor

--- app/assets/javascripts/editing/button_editor.js
+++ app/assets/javascripts/editing/button_editor.js
@@ -3,6 +3,8 @@
 scrivito.on("content", function() { 
   $(document).on("click.button_editor", ".button_editor", function(event) {
     var button = $(event.target);
+    $("input.button_editor").removeClass("active");
+    button.addClass("active");
     button.scrivito('save', button.attr('button-value'));
   }); 
 });
