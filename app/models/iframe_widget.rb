2f1b44a4feb96bee0f8280b1918ca958dd303655
Gert Geidel <gert.geidel@infopark.de>
2015-09-11 16:51:20 +0200

    Default size for iframe

--- app/models/iframe_widget.rb
+++ app/models/iframe_widget.rb
@@ -1,6 +1,6 @@
 class IframeWidget < Widget
-  attribute :width, :string
-  attribute :height, :string
+  attribute :width, :string, default: '100%'
+  attribute :height, :string, default: '425'
   attribute :link, :link
 
 end
